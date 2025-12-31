27195d8051d0812e1e92578b5b6b5d7074d03259
shubhangi-sanjay <ssanjay.scaler@gmail.com>
2025-12-31 19:36:55 +0530

    Update and rename 02_fail_script.sql to 03_fail_script.sql

--- sql/02_fail_script.sql
+++
@@ -1,7 +0,0 @@
--- Intentionally failing script for dependency testing
-
-ALTER TABLE GIT_SCHEMA.EMPLOYEE
-ADD COLUMN ADDRESS VARCHAR(255);
-
--- Run this script twice OR
--- Run after a column already exists → guaranteed failure
